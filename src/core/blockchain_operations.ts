0868486ebd5b05437727adf4351dfb1b72c83363
Rodrigo Quelhas <rodrigo_quelhas@outlook.pt>
2022-02-03 23:04:09 +0000

    feat: Adds enum for blockchain operation. It makes the code easier to maintain

--- src/core/blockchain_operations.ts
+++ src/core/blockchain_operations.ts
@@ -1,49 +1,51 @@
 import { LineInfo } from '../misc/utils';
 import { ILiteral } from '../typings/literal';
+import BlockchainOperations from './enums/blockchain_operations';
 import { Expression } from './expression';
 
 /**
  * @see https://tezos.gitlab.io/michelson-reference/#instr-AMOUNT
  */
-export const GetAmount = () => new Expression('amount');
+export const GetAmount = () => new Expression(BlockchainOperations.amount);
 /**
  * @see https://tezos.gitlab.io/michelson-reference/#instr-BALANCE
  */
-export const GetBalance = () => new Expression('balance');
+export const GetBalance = () => new Expression(BlockchainOperations.balance);
 /**
  * @see https://tezos.gitlab.io/michelson-reference/#instr-CHAIN_ID
  */
-export const GetChain_id = () => new Expression('chain_id');
+export const GetChain_id = () => new Expression(BlockchainOperations.chain_id);
 /**
  * @see https://tezos.gitlab.io/michelson-reference/#instr-LEVEL
  */
-export const GetLevel = () => new Expression('level');
+export const GetLevel = () => new Expression(BlockchainOperations.level);
 /**
  * @see https://tezos.gitlab.io/michelson-reference/#instr-NOW
  */
-export const GetTimestamp = () => new Expression('now');
+export const GetTimestamp = () => new Expression(BlockchainOperations.now);
 /**
  * @see https://tezos.gitlab.io/michelson-reference/#instr-SELF_ADDRESS
  */
-export const GetSelf = (entry_point = '', line = new LineInfo()) => new Expression('self', `"${entry_point}"`, line);
+export const GetSelf = (entry_point = '', line = new LineInfo()) =>
+    new Expression(BlockchainOperations.self, `"${entry_point}"`, line);
 /**
  * @see https://tezos.gitlab.io/michelson-reference/#instr-SELF_ADDRESS
  */
-export const GetSelfAddress = () => new Expression('self_address');
+export const GetSelfAddress = () => new Expression(BlockchainOperations.self_address);
 /**
  * @see https://tezos.gitlab.io/michelson-reference/#instr-SENDER
  */
-export const GetSender = () => new Expression('sender');
+export const GetSender = () => new Expression(BlockchainOperations.sender);
 /**
  * @see https://tezos.gitlab.io/michelson-reference/#instr-SOURCE
  */
-export const GetSource = () => new Expression('source');
+export const GetSource = () => new Expression(BlockchainOperations.source);
 /**
  * @see https://tezos.gitlab.io/michelson-reference/#instr-TOTAL_VOTING_POWER
  */
-export const GetTotalVotingPower = () => new Expression('total_voting_power');
+export const GetTotalVotingPower = () => new Expression(BlockchainOperations.total_voting_power);
 /**
  * @see https://tezos.gitlab.io/michelson-reference/#instr-VOTING_POWER
  */
 export const GetVotingPower = (key_hash: ILiteral<'key_hash'>, line = new LineInfo()) =>
-    new Expression('voting_power', `${key_hash}`, line);
+    new Expression(BlockchainOperations.voting_power, `${key_hash}`, line);
