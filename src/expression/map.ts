012ea77beb33b41ed04a8157daed203758baf903
Rodrigo Quelhas <rodrigo_quelhas@outlook.pt>
2022-03-04 13:53:05 +0000

    doc: Improve typedoc on map expressions

--- src/expression/map.ts
+++ src/expression/map.ts
@@ -4,8 +4,38 @@ import { IExpression } from '../typings/expression';
 import ExpressionAtom from '../core/enums/expression';
 import { Expression } from '../core/expression';
 
+/**
+ * Get map entries.
+ *
+ * ```typescript
+ * GetEntries(Map([ ["a", 1], ["b", 2] ]));
+ * ```
+ *
+ * @param source Map expression
+ * @param {LineInfo} line Source code line information (Used in error messages)
+ *
+ * @returns {IExpression} An expression of type TList(TRecord({ key: ..., value: ... })).
+ */
 export const GetEntries = (source: IExpression, line = new LineInfo()) =>
     proxy(new Expression(ExpressionAtom.items, source, line), Expression.proxyHandler);
+
+/**
+ * Update an entry on map or big map.
+ *
+ * ```typescript
+ * // Update/Insert entry
+ * UpdateMap(ContractStorage().metadata, String("some_key"), Some(Nat(10)));
+ * // Remove entry
+ * UpdateMap(ContractStorage().metadata, String("some_key"), None());
+ * ```
+ *
+ * @param source A map expression.
+ * @param key The map key.
+ * @param value An optional value to be indexed by the given key. (The entry is removed if None() is provided as value)
+ * @param {LineInfo} line Source code line information (Used in error messages)
+ *
+ * @returns {IExpression} An expression
+ */
 export const UpdateMap = (source: IExpression, key: IExpression, value: IExpression, line = new LineInfo()) =>
     proxy(new Expression(ExpressionAtom.update_map, source, key, value, line), Expression.proxyHandler);
 
