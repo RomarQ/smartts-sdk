eb84d93c12a65444aae3caf4791ab36609964015
Rodrigo Quelhas <rodrigo_quelhas@outlook.pt>
2022-03-04 16:43:42 +0000

    feat: Adds (transfer) entrypoint to FA2 template

--- tests/templates/FA2.ts
+++ tests/templates/FA2.ts
@@ -24,6 +24,9 @@ import {
     Mutez,
     MapContainsKey,
     PrependToList,
+    Or,
+    GreaterThanOrEqual,
+    CastToNat,
 } from '../../src/expression';
 import {
     TAddress,
@@ -49,6 +52,8 @@ enum FA2_Error {
     NOT_OWNER = 'FA2__NOT_OWNER',
     PAUSED = 'FA2__PAUSED',
     TOKEN_UNDEFINED = 'FA2__TOKEN_UNDEFINED',
+    NOT_OPERATOR = 'FA2__NOT_OPERATOR',
+    INSUFFICIENT_BALANCE = 'FA2__INSUFFICIENT_BALANCE',
 }
 
 /**
@@ -76,69 +81,91 @@ const TTokenMetadata = TRecord(
     },
     Layout.right_comb,
 );
-// "mint" Entrypoint argument
-const TEntrypointMint = TRecord(
-    {
-        address: TAddress(),
-        amount: TNat(),
-        token_id: TNat(),
-    },
-    Layout.right_comb,
-);
-// "update_operators" Entrypoint argument
-const TEntrypointUpdateOperators = TList(
-    TVariant(
-        {
-            add_operator: TOperatorKey,
-            remove_operator: TOperatorKey,
-        },
-        ['add_operator', 'remove_operator'],
-    ),
-);
-// "balance_of" Entrypoint argument
-const TEntrypointBalanceOf = TRecord(
-    {
-        requests: TList(
-            TRecord(
-                {
-                    owner: TAddress(),
-                    token_id: TNat(),
-                },
-                ['owner', 'token_id'],
-            ),
+
+class EntrypointTypes {
+    // "transfer"
+    static Transfer = TList(
+        TRecord(
+            {
+                from_: TAddress(),
+                txs: TList(
+                    TRecord(
+                        {
+                            to_: TAddress(),
+                            token_id: TNat(),
+                            amount: TNat(),
+                        },
+                        ['to_', ['token_id', 'amount']],
+                    ),
+                ),
+            },
+            ['from_', 'txs'],
         ),
-        callback: TContract(
-            TList(
+    );
+    // "update_operators"
+    static UpdateOperators = TList(
+        TVariant(
+            {
+                add_operator: TOperatorKey,
+                remove_operator: TOperatorKey,
+            },
+            ['add_operator', 'remove_operator'],
+        ),
+    );
+    // "balance_of"
+    static BalanceOf = TRecord(
+        {
+            requests: TList(
                 TRecord(
                     {
-                        request: TRecord(
-                            {
-                                owner: TAddress(),
-                                token_id: TNat(),
-                            },
-                            ['owner', 'token_id'],
-                        ),
-                        balance: TNat(),
+                        owner: TAddress(),
+                        token_id: TNat(),
                     },
-                    ['request', 'balance'],
+                    ['owner', 'token_id'],
                 ),
             ),
-        ),
-    },
-    ['requests', 'callback'],
-);
+            callback: TContract(
+                TList(
+                    TRecord(
+                        {
+                            request: TRecord(
+                                {
+                                    owner: TAddress(),
+                                    token_id: TNat(),
+                                },
+                                ['owner', 'token_id'],
+                            ),
+                            balance: TNat(),
+                        },
+                        ['request', 'balance'],
+                    ),
+                ),
+            ),
+        },
+        ['requests', 'callback'],
+    );
+    // "mint"
+    static Mint = TRecord(
+        {
+            address: TAddress(),
+            amount: TNat(),
+            token_id: TNat(),
+        },
+        Layout.right_comb,
+    );
+}
 
 /**
  * Common Expressions
  */
+class CommonExpressions {
+    // Terminate contract execution and fail if the sender is not the administrator
+    static FailIfSenderIsNotAdmin = () =>
+        Require(Equal(GetSender(), ContractStorage().config.administrator), String(FA2_Error.NOT_ADMIN));
 
-// Terminate contract execution and fail if the sender is not the administrator
-const FailIfSenderIsNotAdmin = () =>
-    Require(Equal(GetSender(), ContractStorage().config.administrator), String(FA2_Error.NOT_ADMIN));
-
-// Terminate contract execution and fail if the contract is paused
-const FailIfContractIsPaused = () => Require(Not(ContractStorage().config.paused), String(FA2_Error.PAUSED));
-
+    // Terminate contract execution and fail if the contract is paused
+    static FailIfContractIsPaused = () => Require(Not(ContractStorage().config.paused), String(FA2_Error.PAUSED));
+}
 const FA2Contract = new Contract()
     .setStorageType(
         TRecord(
@@ -173,11 +200,75 @@ const FA2Contract = new Contract()
             metadata: Big_map(),
         }),
     )
-    .addEntrypoint(new EntryPoint('transfer'))
     .addEntrypoint(
-        new EntryPoint('update_operators').inputType(TEntrypointUpdateOperators).code((entrypoint_arg) => [
+        new EntryPoint('transfer').inputType(EntrypointTypes.Transfer).code((entrypoint_arg) => [
             // The contract must not be paused
-            FailIfContractIsPaused(),
+            CommonExpressions.FailIfContractIsPaused(),
+            // Iterate over each transfer request
+            ForEachOf(entrypoint_arg).Do((transfer) => [
+                // Iterate over each transaction
+                ForEachOf(transfer.txs).Do((transaction) => [
+                    // Fail if the token does not exist
+                    Require(
+                        MapContainsKey(ContractStorage().assets.token_total_supply, transaction.token_id),
+                        String(FA2_Error.TOKEN_UNDEFINED),
+                    ),
+                    // Verify if sender has permissions
+                    Require(
+                        Or(
+                            Comparison.Equal(GetSender(), transfer.from_),
+                            MapContainsKey(
+                                ContractStorage().assets.operators,
+                                Record({
+                                    owner: transfer.from_,
+                                    operator: GetSender(),
+                                    token_id: transaction.token_id,
+                                }),
+                            ),
+                        ),
+                        String(FA2_Error.NOT_OPERATOR),
+                    ),
+                    // Decrement balance from the source address
+                    NewVariable('sender_ledger_key', Pair(transfer.from_, transaction.token_id)),
+                    NewVariable(
+                        'sender_balance',
+                        GetMapValue(
+                            ContractStorage().assets.ledger,
+                            GetVariable('sender_ledger_key'),
+                            Record({ balance: Nat(0) }),
+                        ).balance,
+                    ),
+                    SetValue(
+                        GetMapValue(ContractStorage().assets.ledger, GetVariable('sender_ledger_key')),
+                        Record({
+                            balance: CastToNat(
+                                Math.Subtract(GetVariable('sender_balance'), transaction.amount),
+                                String(FA2_Error.INSUFFICIENT_BALANCE),
+                            ),
+                        }),
+                    ),
+                    // Increment balance to the recipient address
+                    NewVariable('recipient_ledger_key', Pair(transaction.to_, transaction.token_id)),
+                    NewVariable(
+                        'recipient_balance',
+                        GetMapValue(
+                            ContractStorage().assets.ledger,
+                            GetVariable('recipient_ledger_key'),
+                            Record({ balance: Nat(0) }),
+                        ).balance,
+                    ),
+                    SetValue(
+                        GetMapValue(ContractStorage().assets.ledger, GetVariable('recipient_ledger_key')),
+                        Record({ balance: Math.Add(GetVariable('recipient_balance'), transaction.amount) }),
+                    ),
+                ]),
+            ]),
+        ]),
+    )
+    .addEntrypoint(
+        new EntryPoint('update_operators').inputType(EntrypointTypes.UpdateOperators).code((entrypoint_arg) => [
+            // The contract must not be paused
+            CommonExpressions.FailIfContractIsPaused(),
             // Apply actions
             ForEachOf(entrypoint_arg).Do((item) => [
                 MatchVariant(item)
@@ -193,9 +284,9 @@ const FA2Contract = new Contract()
         ]),
     )
     .addEntrypoint(
-        new EntryPoint('balance_of').inputType(TEntrypointBalanceOf).code((entrypoint_arg) => [
+        new EntryPoint('balance_of').inputType(EntrypointTypes.BalanceOf).code((entrypoint_arg) => [
             // Fail if contract is paused
-            FailIfContractIsPaused(),
+            CommonExpressions.FailIfContractIsPaused(),
             // Iterate over each request "list(requests)" and compute responses
             NewVariable('responses', List([])),
             ForEachOf(entrypoint_arg.requests).Do((request) => [
@@ -224,10 +315,10 @@ const FA2Contract = new Contract()
         ]),
     )
     .addEntrypoint(
-        new EntryPoint('mint').inputType(TEntrypointMint).code((arg) => [
+        new EntryPoint('mint').inputType(EntrypointTypes.Mint).code((entrypoint_arg) => [
             // Sender must be the administrator
-            FailIfSenderIsNotAdmin(),
-            NewVariable('ledger_key', Pair(arg.address, arg.token_id)),
+            CommonExpressions.FailIfSenderIsNotAdmin(),
+            NewVariable('ledger_key', Pair(entrypoint_arg.address, entrypoint_arg.token_id)),
             NewVariable(
                 'balance',
                 GetMapValue(ContractStorage().assets.ledger, GetVariable('ledger_key'), Record({ balance: Nat(0) }))
@@ -235,14 +326,14 @@ const FA2Contract = new Contract()
             ),
             SetValue(
                 GetMapValue(ContractStorage().assets.ledger, GetVariable('ledger_key')),
-                Record({ balance: Math.Add(GetVariable('balance'), arg.amount) }),
+                Record({ balance: Math.Add(GetVariable('balance'), entrypoint_arg.amount) }),
             ),
         ]),
     )
     .addEntrypoint(
         new EntryPoint('pause').inputType(TBool()).code((paused) => [
             // Sender must be the administrator
-            FailIfSenderIsNotAdmin(),
+            CommonExpressions.FailIfSenderIsNotAdmin(),
             // Update paused state
             SetValue(ContractStorage().config.paused, paused),
         ]),
@@ -250,7 +341,7 @@ const FA2Contract = new Contract()
     .addEntrypoint(
         new EntryPoint('set_admin').inputType(TAddress()).code((address) => [
             // Sender must be the administrator
-            FailIfSenderIsNotAdmin(),
+            CommonExpressions.FailIfSenderIsNotAdmin(),
             // Update administrator address
             SetValue(ContractStorage().config.administrator, address),
         ]),
@@ -258,7 +349,7 @@ const FA2Contract = new Contract()
     .addEntrypoint(
         new EntryPoint('update_metadata').inputType(TMap(TString(), TBytes())).code((metadata) => [
             // Sender must be the administrator
-            FailIfSenderIsNotAdmin(),
+            CommonExpressions.FailIfSenderIsNotAdmin(),
             // Update metadata entries
             ForEachOf(GetMapEntries(metadata)).Do((item) => [
                 SetValue(GetMapValue(ContractStorage().metadata, item.key), item.value),
