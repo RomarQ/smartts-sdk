c89bd4c1271d0dbfbb3b8093c21f50350ec510ac
Rodrigo Quelhas <rodrigo_quelhas@outlook.pt>
2022-03-16 17:33:50 +0000

    feat: Allow customization when providing LineInfo on expressions/statements and updates SmartML

--- src/misc/utils.ts
+++ src/misc/utils.ts
@@ -8,14 +8,16 @@ export const capitalizeBoolean = (bool: boolean): string => (bool ? Prim.True : 
 
 export class LineInfo implements IToString {
     constructor(public fileName = '', public lineNumber = '-1') {
-        const stack = new Error().stack || '';
-        const matches = stack.match(/Object.*[(](.+):(\d+):(\d+)[)]/);
-        if (matches) {
-            if (matches.length > 0) {
-                this.fileName = matches[1].split('/').reverse()[0];
-            }
-            if (matches.length > 1) {
-                this.lineNumber = matches[2];
+        if (!fileName) {
+            const stack = new Error().stack || '';
+            const matches = stack.match(/Object.*[(](.+):(\d+):(\d+)[)]/);
+            if (matches) {
+                if (matches.length > 0) {
+                    this.fileName = matches[1].split('/').reverse()[0];
+                }
+                if (matches.length > 1) {
+                    this.lineNumber = matches[2];
+                }
             }
         }
     }
